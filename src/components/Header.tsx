67607bf72291df1481086c68b75e6d61a037b37e
lutfi <mlutfiibra@outlook.com>
2023-08-08 16:23:42 +0700

    Implementing hyperlink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const MyHeader: React.FC = () => {
     return (
       <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-2xl font-semibold">FE Test</h1>
+        <div className="container flex flex-row items-center">
+            <h1 className="text-2xl font-semibold">FE Test</h1>
+
+            <p className='m-0 mx-4'><Link to="/dashboard">Dashboard</Link></p>
+            <p className='m-0'><Link to="/master-data">Master Data</Link></p>
+        </div>
       </header>
     );
   };
